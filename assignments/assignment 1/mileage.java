ba541e57ece6b182751bae2d0fbf3f9788167eeb
janessa franklin <janessafranklin@janessas-MacBook-Air.local>
2026-01-18 17:43:11 -0500

    updates

---
+++ assignments/assignment 1/mileage.java
@@ -0,0 +1,29 @@
+package mileage;
+
+public class mileage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+        double miles = 300; // distance traveled
+        double gallons = 12.3; // gallons of gas used
+        		
+        
+        double mpg = miles / gallons; // calculating miles per gallon
+	    System.out.println("Miles per gallon: " + mpg);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
